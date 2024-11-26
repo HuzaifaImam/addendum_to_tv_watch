c901cc0b285f1e97d0d6ebc104ca1c400ab518d9
Huzaifa Shehu Imam <huzaifashehu@live.com>
2024-11-26 01:34:02 +0100

    above 35 alone

--- mainfile.R
+++ mainfile.R
@@ -2,7 +2,7 @@ if(!require("pacman"))install.packages("pacman")
 pacman::p_load(tidyverse, here)
 gss_cat
 tv_watch_hours_table <- gss_cat %>% 
-  filter(age >= 40) %>% 
+  filter(age > 35) %>% 
   group_by(marital) %>% 
   summarize(tv_watch_mean = mean(tvhours, na.rm = TRUE))
 write.csv(tv_watch_hours_table, here("TV_hours_by_marital_status.csv"))
